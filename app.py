3ccc9e0f58cba8611cf245a2f6dbff5af46dcf94
shlai <shihong30@gmail.com>
2024-10-13 05:04:49 +0800

    Update app.py

--- app.py
+++ app.py
@@ -50,4 +50,4 @@ def analyze_sentiment():
     return jsonify({'sentiment': sentiment})
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
